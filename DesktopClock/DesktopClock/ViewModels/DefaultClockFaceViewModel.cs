2c24b365428b94b633555d2ecb41af3e7648abfc
ppsz <patrycjusz.szydlik@gmail.com>
2019-04-19 00:08:35 +0200

    Implemented beginning of DefaultClockFaceViewModel

--- DesktopClock/DesktopClock/ViewModels/DefaultClockFaceViewModel.cs
+++ DesktopClock/DesktopClock/ViewModels/DefaultClockFaceViewModel.cs
@@ -8,5 +8,43 @@ namespace DesktopClock.ViewModels
 {
     class DefaultClockFaceViewModel : ViewModelBase
     {
+        #region Fields
+        #endregion
+
+        #region Properties
+        #region BackgroundOpacity Property
+        private float _backgroundOpacity;
+        public float BackgroundOpacity
+        {
+            get => _backgroundOpacity;
+            set => SetField(out _backgroundOpacity, value);
+        }
+        #endregion
+        #region CurrentDate Property
+        private string _currentDate;
+        public string CurrentDate
+        {
+            get => _currentDate;
+            set => SetField(out _currentDate, value);
+        }
+        #endregion
+        #endregion
+
+        #region Constructors
+        public DefaultClockFaceViewModel()
+        {
+            BackgroundOpacity = 0.25f;
+            var date = DateTime.Now;
+            CurrentDate = $"{date.ToString("dd MMMM yyyy")}{Environment.NewLine}{date.ToString("dddd")}";
+        }
+        #endregion
+
+        #region Commands
+        #endregion
+
+        #region Methods
+        #endregion
+
+
     }
 }
